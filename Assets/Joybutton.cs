00d6a5aaf9c02535e3568381f8d6df20ae8b1b44
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-06 13:44:35 +0100

    Chore: change from joystick to next and previous button

--- Assets/Joybutton.cs
+++ Assets/Joybutton.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Joybutton : MonoBehaviour {
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
 }
